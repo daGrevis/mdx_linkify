1b662ef9efc5b8b81b7efc15e6bc774f17577f49
Raitis Stengrevics <dagrevis@gmail.com>
2013-02-16 22:05:48 +0200

    Rewritten all from scratch so it doesn't use any regexes.

--- mdx_linkify.py
+++ mdx_linkify.py
@@ -1,24 +1,36 @@
-from markdown.inlinepatterns import Pattern
-from markdown.util import etree, AtomicString
+# -*- coding: utf-8 -*-
+import string
+
+from urlparse import urlparse
+
+from markdown.preprocessors import Preprocessor
 from markdown import Extension
 
 
-URL_RE = r"\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))"
-# http://daringfireball.net/2009/11/liberal_regex_for_matching_urls
-
-
-class LinkifyPattern(Pattern):
-    def handleMatch(self, match):
-        match = match.group(0)
-        el = etree.Element("a")
-        el.set("href", match)
-        el.text = AtomicString(match)
-        return el
+class LinkifyPreprocessor(Preprocessor):
+    def run(self, lines):
+        new_lines = []
+        for line in lines:
+            new_words = []
+            for word in line.split(" "):
+                if word == "":
+                    continue
+                if word[0] not in string.ascii_letters:
+                    new_words.append(word)
+                    continue
+                parsed_url = urlparse(word)
+                if parsed_url.scheme not in ["http", "https"]:
+                    new_words.append(word)
+                    continue
+                link = "[{}]({})".format(word, word)
+                new_words.append(link)
+            new_lines.append(" ".join(new_words))
+        return new_lines
 
 
 class LinkifyExtension(Extension):
     def extendMarkdown(self, md, md_globals):
-        md.inlinePatterns["autolink"] = LinkifyPattern(URL_RE, md)
+        md.preprocessors.add("linkify", LinkifyPreprocessor(md), "_begin")
 
 
 def makeExtension(configs=None):
--- tests.py
+++ tests.py
@@ -5,13 +5,34 @@ from markdown import markdown
 
 class LinkifyTest(unittest.TestCase):
     def test_link(self):
-        expected = '<p><a href="http://example.org/">http://example.org/</a></p>'
-        actual = markdown("http://example.org/", extensions=["linkify"])
+        expected = '<p><a href="http://example.com">http://example.com</a></p>'
+        actual = markdown("http://example.com", extensions=["linkify"])
         self.assertEqual(expected, actual)
 
     def test_no_link(self):
-        expected = '<p>example.org</p>'
-        actual = markdown("example.org", extensions=["linkify"])
+        expected = '<p>example.com</p>'
+        actual = markdown("example.com", extensions=["linkify"])
+        self.assertEqual(expected, actual)
+
+    def test_links(self):
+        expected = ('<p><a href="http://example.com">http://example.com</a> '
+                    '<a href="http://example.org">http://example.org</a></p>')
+        actual = markdown("http://example.com http://example.org",
+                          extensions=["linkify"])
+        self.assertEqual(expected, actual)
+
+    def test_links_with_text_between(self):
+        expected = ('<p><a href="http://example.com">http://example.com</a> '
+                    'foo <a href="http://example.org">http://example.org'
+                    '</a></p>')
+        actual = markdown("http://example.com foo http://example.org",
+                          extensions=["linkify"])
+        self.assertEqual(expected, actual)
+
+    def test_existing_link(self):
+        expected = '<p><a href="http://example.com">http://example.com</a></p>'
+        actual = markdown("[http://example.com](http://example.com)",
+                          extensions=["linkify"])
         self.assertEqual(expected, actual)
 
 
