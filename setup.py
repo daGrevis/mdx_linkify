362c6df73f2ce8f399ad6ffd99c5be840c9fe113
Raitis Stengrevics <dagrevis@gmail.com>
2014-03-01 10:58:22 +0200

    It's 2014 and I how some understanding of what I'm doing

--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
-#!/usr/bin/env python2
-from distutils.core import setup
+import os.path as path
+from setuptools import setup
 
+
+def get_readme(filename):
+    with open(path.join(path.dirname(__file__), filename)) as readme:
+        content = readme.read()
+    return content
 
 setup(name="mdx_linkify",
-      version="0.2",
-      description="Link recognition for Python Markdown",
+      version="0.3",
       author="Raitis (daGrevis) Stengrevics",
       author_email="dagrevis@gmail.com",
+      description="Link recognition for Python Markdown",
+      license="MIT",
+      keywords="markdown links",
       url="https://github.com/daGrevis/mdx_linkify",
       packages=["mdx_linkify"],
-      install_requires=["Markdown>=2.4", "bleach>=1.4"])
+      long_description=get_readme("README.md"),
+      classifiers=[
+          "Topic :: Text Processing :: Markup",
+          "Topic :: Utilities",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: Implementation :: PyPy",
+          "Development Status :: 4 - Beta",
+          "License :: OSI Approved :: MIT License",
+      ],
+      install_requires=["Markdown>=2.4", "bleach>=1.4"],
+      test_suite="mdx_linkify.tests")
