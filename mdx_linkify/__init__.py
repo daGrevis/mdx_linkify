08335e060311994a897b95302fc54a0a2b196614
Raitis Stengrevics <dagrevis@gmail.com>
2014-03-01 11:27:29 +0200

    Fix import for python2 and pypy

--- mdx_linkify/__init__.py
+++ mdx_linkify/__init__.py
@@ -1,1 +1,12 @@
-from mdx_linkify.mdx_linkify import makeExtension
+import sys
+
+
+is_python3 = sys.version_info >= (3, 0)
+
+if is_python3:
+    from mdx_linkify.mdx_linkify import makeExtension
+else:
+    from mdx_linkify import makeExtension
+
+
+assert makeExtension  # Silences pep8.
