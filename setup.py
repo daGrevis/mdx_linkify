33bb11c0691bfb5b3a14d11c0eb6202f5905e01c
Raitis Stengrevics <dagrevis@gmail.com>
2020-01-11 18:44:32 +0200

    Release v1.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_readme(filename):
     return content
 
 setup(name="mdx_linkify",
-      version="1.2",
+      version="1.3",
       author="Raitis (daGrevis) Stengrevics",
       author_email="dagrevis@gmail.com",
       description="Link recognition for Python Markdown",
